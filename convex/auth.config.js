e10aa9c020e63a608fcac496abc4df29d5b0dcfc
sondeokhyeon <sondeokhyeon@gmail.com>
2023-12-26 00:11:38 +0900

    Update : Authentication domain Change

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://festive-mole-37.convex.cloud",
+      domain: "https://relevant-cardinal-22.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],

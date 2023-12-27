19774a96259bcfe54fe9ab15ba1cb4d2726f0032
sondeokhyeon <sondeokhyeon@gmail.com>
2023-12-27 23:07:18 +0900

    Add : Dummy Text Page Add

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,11 @@
+const DocumentsPage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nemo
+      quaerat minus laboriosam ex? Sunt, perferendis aspernatur quidem maiores
+      est quam nemo iusto fugit neque deleniti? Officia ea quasi eveniet?
+    </div>
+  );
+};
+
+export default DocumentsPage;

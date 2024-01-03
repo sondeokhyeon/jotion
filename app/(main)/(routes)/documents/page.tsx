8aaeacebba9dc9186c971fe486bb4afe10edbe21
sondeokhyeon <sondeokhyeon@gmail.com>
2024-01-03 22:23:36 +0900

    Update : page - documents

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+
 const DocumentsPage = () => {
+  const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created",
+      erorr: "Failed to create a new note.",
+    });
+  };
+
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nemo
-      quaerat minus laboriosam ex? Sunt, perferendis aspernatur quidem maiores
-      est quam nemo iusto fugit neque deleniti? Officia ea quasi eveniet?
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button onClick={onCreate}>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
     </div>
   );
 };

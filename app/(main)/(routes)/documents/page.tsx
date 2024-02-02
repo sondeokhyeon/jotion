c5dd0d93eb84e54c9deffbad9752298a3515fe52
sondeokhyeon <sondeokhyeon@gmail.com>
2024-02-02 23:55:58 +0900

    Refact : typo apply

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -17,7 +17,7 @@ const DocumentsPage = () => {
     toast.promise(promise, {
       loading: "Creating a new note...",
       success: "New note created",
-      erorr: "Failed to create a new note.",
+      error: "Failed to create a new note.",
     });
   };
 
